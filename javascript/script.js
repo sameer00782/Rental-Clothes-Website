977e4c2b0f012257f0d080fa9f4c32edd7027e1b
Sameer Sonwane <sameersonwane08@gmail.com>
2024-01-24 11:51:08 +0530

    new changes

--- javascript/script.js
+++ javascript/script.js
@@ -32,6 +32,38 @@ window.addEventListener('scroll', () => {
 
 
 
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+
+
 const allHoverImages = document.querySelectorAll('.hover-container div img');
 const imgContainer = document.querySelector('.img-container');
 
@@ -52,9 +84,6 @@ function resetActiveImg(){
         img.parentElement.classList.remove('active');
     });
 }
-
-
- 
 
  const product =[
     {
@@ -131,3 +160,6 @@ function displaycart(c){
     }
 }
 displaycart();
+
+
+
