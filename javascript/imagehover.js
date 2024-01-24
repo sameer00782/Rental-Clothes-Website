29485ebaebfacbf4d68be2d08d0ce0c78054bdff
Sameer Sonwane <sameersonwane08@gmail.com>
2024-01-24 12:37:03 +0530

    new changes added

---
+++ javascript/imagehover.js
@@ -0,0 +1,53 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = ()  => {
+    search.classList.toggle('active');
+    menu.classList.remove('active');
+}
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = ()  => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+// Hide Menu And Search Box On Scroll
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+
+}
+
+
+
+//header
+let header = document.querySelector('header');
+
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+
+});
+
+
+const allHoverImages = document.querySelectorAll('.hover-container div img');
+const imgContainer = document.querySelector('.img-container');
+
+window.addEventListener('DOMContentLoaded', () => {
+    allHoverImages[0].parentElement.classList.add('active');
+});
+
+allHoverImages.forEach((image) => {
+    image.addEventListener('mouseover', () =>{
+        imgContainer.querySelector('img').src = image.src;
+        resetActiveImg();
+        image.parentElement.classList.add('active');
+    });
+});
+
+function resetActiveImg(){
+    allHoverImages.forEach((img) => {
+        img.parentElement.classList.remove('active');
+    });
+}
